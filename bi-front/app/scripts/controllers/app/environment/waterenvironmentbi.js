c8bd14aa914f679c264a5be1f5d54d9d1ee2170e
ZhaoZhenhua <1196464392@qq.com>
2015-12-14 10:20:02 +0800

    加点标记

--- bi-front/app/scripts/controllers/app/environment/waterenvironmentbi.js
+++ bi-front/app/scripts/controllers/app/environment/waterenvironmentbi.js
@@ -7,8 +7,10 @@ app.controller('WaterCtrl', ['$scope', function($scope) {
     var map = new AMap.Map('map_wMap',{
         zoom: 10,
         center: [121.106661, 31.579533]
-
-
+    });
+     var marker = new AMap.Marker({
+        position: [121.106661,  31.579533],
+        map:map
     });
 
     var waterQualityPredict = [4004,4003,4034]
