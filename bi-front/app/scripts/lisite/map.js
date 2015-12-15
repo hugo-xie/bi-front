d1d1bb88c756c124d52c5496d2a30ec01f33de2b
LISITE <8lisite@qq.com>
2015-12-15 19:53:39 +0800

    123

--- bi-front/app/scripts/lisite/map.js
+++ bi-front/app/scripts/lisite/map.js
@@ -19,18 +19,42 @@ var marker3 = new AMap.Marker({
 marker3.setMap(map);
 //显示信息窗体(可单击位置显示隐藏的信息窗体)
 var infowindow1 = new AMap.InfoWindow({
-     content: '<h4>第二水厂</h4>',
+     content: "<h4>第二水厂</h4>\
+    <table class='lst-table' width='100%' style='font-size: 50%'>\
+        <tr class='lst-tr'>\
+            <th class='lst-th'>溶解氧</th>\
+            <th class='lst-th'>高锰酸钾</th>\
+            <th class='lst-th'>氨氮</th>\
+        </tr>\
+        <tr class='lst-tr'>\
+            <td class='lst-td'>34</td>\
+            <td class='lst-td'>56</td>\
+            <td class='lst-td'>40</td>\
+        </tr>\
+    </table>",
      offset: new AMap.Pixel(0, -30),
-     size:new AMap.Size(150,0)
+     size:new AMap.Size(200,0)
 })
 var clickHandle1 = AMap.event.addListener(marker1, 'click', function() {
     infowindow1.open(map, marker1.getPosition())
 })
 
 var infowindow2 = new AMap.InfoWindow({
-    content: "<h4>协鑫码头</h4><div style='color:red'>123</div>",
+    content: "<h4>协鑫码头</h4>\
+    <table class='lst-table' width='100%' style='font-size: 50%'>\
+        <tr class='lst-tr'>\
+            <th class='lst-th'>溶解氧</th>\
+            <th class='lst-th'>高锰酸钾</th>\
+            <th class='lst-th'>氨氮</th>\
+        </tr>\
+        <tr class='lst-tr'>\
+            <td class='lst-td'>34</td>\
+            <td class='lst-td'>56</td>\
+            <td class='lst-td'>40</td>\
+        </tr>\
+    </table>",
      offset: new AMap.Pixel(0, -30),
-     size:new AMap.Size(150,0)
+     size:new AMap.Size(200,0)
 })
 infowindow2.open(map, marker2.getPosition());
 var clickHandle2 = AMap.event.addListener(marker2, 'click', function() {
@@ -38,9 +62,21 @@ var clickHandle2 = AMap.event.addListener(marker2, 'click', function() {
 })
 
 var infowindow3 = new AMap.InfoWindow({
-     content: '<h4>第三水厂</h4>',
+     content: "<h4>第三水厂</h4>\
+    <table class='lst-table' width='100%' style='font-size: 50%'>\
+        <tr class='lst-tr'>\
+            <th class='lst-th'>溶解氧</th>\
+            <th class='lst-th'>高锰酸钾</th>\
+            <th class='lst-th'>氨氮</th>\
+        </tr>\
+        <tr class='lst-tr'>\
+            <td class='lst-td'>34</td>\
+            <td class='lst-td'>56</td>\
+            <td class='lst-td'>40</td>\
+        </tr>\
+    </table>",
      offset: new AMap.Pixel(0, -30),
-     size:new AMap.Size(150,0)
+     size:new AMap.Size(200,0)
 })
 var clickHandle3 = AMap.event.addListener(marker3, 'click', function() {
     infowindow3.open(map, marker3.getPosition())
